de34fab4769e55d1b85550f98b27ced9d66080c3
Sofia <kulchitskasofia@gmail.com>
2022-11-10 20:38:15 +0200

    second test

---
+++ UnitTest3/UnitTest3.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba5.3/laba5.3.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace UnitTest3
+{
+	TEST_CLASS(UnitTest3)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			double a;
+			a = k(0.1);
+			Assert::AreEqual(a, 0.);
+		}
+	};
+}
